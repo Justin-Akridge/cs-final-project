970b5004c7fe414a6d8d65bf79b6d83ce3ab2652
Justin Akridge <Akridjus@gmail.com>
2023-03-08 14:05:40 -0500

    Complete function tests on swap, reset, and specialconditions

--- finalproject.cpp
+++ finalproject.cpp
@@ -52,73 +52,79 @@ int first_roll() {
   }
  }
 
-bool check_for_player(int dice_roll, int index, int board[], int positions[]) { 
-  int len = sizeof(positions) / sizeof(positions[0]);
-  for(int i=0; i<len; i++) {
-    if (board[positions[index] + dice_roll] == 1) {
-      return true;
-    }
-    return false;
-  }
-}
+// bool check_for_player(int dice_roll, int index, int board[], int positions_of_players[]) { 
+//   int len = sizeof(positions_of_players);
+//   for(int i=0; i<len; i++) {
+//     if (board[positions_of_players[index] + dice_roll] == 1) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
 
-void start_over(int index, int board[], int positions[]) {
-  board[positions[index]] = 0;
-  positions[index] = 0;
-}
 
-void swap_for_leader(int index, int positions_of_players[]) {
-  int number_of_players = sizeof(positions_of_players) / sizeof(positions_of_players[0]);
+//[x] unit tests for swap_for_lead function completed
+
+void swap_for_leader(int index, int positions_of_players[], int players) {
   int flag = false;
   int position_of_leader = positions_of_players[index];
   int player_in_the_lead = index;
     //find if there is a leading player
-  for (int i=0; i<number_of_players; i++) {
+  for (int i=0; i<players; i++) {
     if (positions_of_players[i] > position_of_leader) {
       position_of_leader = positions_of_players[i];
-      flag = true
+      flag = true;
       player_in_the_lead = i;
     }
   }
-
   // if flag remains false then the player is in the lead
   if (flag == false) {
     cout << "You are the leader. Stay where you are.\n";
     return;
   }
-  
   // this is the position of our leading player
-  int temp = positions_of_players[position_in_the_lead];
+  int temp = positions_of_players[player_in_the_lead];
  //swap the leader with the current player
   positions_of_players[player_in_the_lead] = positions_of_players[index];
   positions_of_players[index] = temp;
 }
 
+//TODO
+//[x]unit test for swap_for_last function
 
-//unit test for swap_for_last function
-
-void swap_for_last(int index, int positions_of_players[]) {
-  int numbers_of_players = sizeof(positions_of_players) / sizeof(positions_of_players[0]);
+void swap_for_last(int index, int positions_of_players[], int players) {
   int flag = false;
   int position_of_last_place = positions_of_players[index];
   int player_in_last_place = index;
     //find if there is a leading player
-  for (int i=0; i<numbers_of_players; i++) {
+  for (int i=0; i<players; i++) {
     if (positions_of_players[i] < position_of_last_place) {
       flag = true;
       player_in_last_place = i;
       position_of_last_place = positions_of_players[i];
     }
   }
-
   // if flag remains false then the player is in the lead
   if (flag == false) {
-    cout << "You are already in last.\n";
-    break;
+    cout << "You are already in last. Stay where you are.\n";
+    return;
   }
+  // swap players position with last place
+  int temp = positions_of_players[player_in_last_place];
+  positions_of_players[player_in_last_place] = positions_of_players[index];
+  positions_of_players[index] = temp;
 }
 
-int special_conditions(int dice_roll, int index, int positions_of_players[]) {
+//TODO
+//[x] UNIT TEST FOR START_OVER
+// THIS FUNCTION WILL RESET PLAYER BACK TO POSITION 0
+void start_over(int index, int positions_of_players[], int board[]) {
+  board[positions_of_players[index]] = 0;
+  positions_of_players[index] -= positions_of_players[index];
+}
+
+
+int special_conditions(int dice_roll, int index, int positions_of_players[], int players, int board[]) {
   switch(dice_roll) {
     case 2:
       return 2;
@@ -136,7 +142,7 @@ int special_conditions(int dice_roll, int index, int positions_of_players[]) {
       return 6;
       break;
     case 7:
-      swap_for_leader(index, positions_of_players);
+      swap_for_leader(index, positions_of_players, players);
       break;
     case 8:
       return 8;
@@ -148,29 +154,36 @@ int special_conditions(int dice_roll, int index, int positions_of_players[]) {
       return 10;
       break;
     case 11:
-      swap_for_last(index, dice_roll, board, positions_of_players);
+      swap_for_last(index, positions_of_players, players);
       break;
     case 12:
-      return 0;
-      // [_] TODO!! FINSIH FUNCTION CALL
-      //call function start over after return statement
+      start_over(index, positions_of_players, board);
       break;
   }
 }
+
 int main() {
   // get players
-  int indexes = get_total_players(); 
+  int players = get_total_players(); 
 
   /* this arrary will keep track of the player (index + 1)
    * and the position on the board where each player is at */
-  int positions_of_players[indexes] = {0};
+  int positions_of_players[players] = {10, 0, 0, 0};
 
+  
   /* Create board.
    * When a player lands on a space, we will change the space
    * from 0 to 1 to mark that a player is there */
   int board[50] = {0};
   
-  special_conditions(7, 0, positions_of_players)
+  special_conditions(12, 0, positions_of_players, players, board);
+  
+  for (auto x: positions_of_players) {
+    cout << x << endl;
+  }
+
+//NOTES:
+//WHENEVER YOU NEED TO DECLARE PLAYERS ADD ONE TO PLAYER INDEX.
 
   //game loop
   // while (board[50] != 1) {
